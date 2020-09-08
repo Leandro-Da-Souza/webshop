328130edf10cf15f6d2577e53806ead9fcfa2101
Leandro Da Souza <dasouza.leandro@gmail.com>
2020-09-08 09:59:36 +0200

    add v-if v-else block in cart.vue

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,29 +1,29 @@
 <template>
   <div>
     <!-- Cart Item start  should this be a component instead ? -->
-    <img src alt />
-    <ul>
-      <li v-for="item in cartItem" :key="item.id">
-        <img :src="item.img" alt />
-        <h3>{{ item.name }}</h3>
-        <p>{{ item.desc }}</p>
-        <p>{{ item.price }} $</p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Dignissimos, qui eligendi blanditiis sed provident excepturi
-          esse nihil, iste ullam, voluptatibus odit totam itaque.
-          Autem quas perferendis facilis alias id nobis!
-        </p>
-        <button @click="deleteItem(item.id)">X</button>
-      </li>
-    </ul>
+    <section v-if="cartItem.length > 0">
+      <ul>
+        <li v-for="item in cartItem" :key="item.id">
+          <img :src="item.img" alt />
+          <h3>{{ item.name }}</h3>
+          <p>{{ item.desc }}</p>
+          <p>{{ item.price }} $</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Dignissimos, qui eligendi blanditiis sed provident excepturi
+            esse nihil, iste ullam, voluptatibus odit totam itaque.
+            Autem quas perferendis facilis alias id nobis!
+          </p>
+          <button @click="deleteItem(item.id)">X</button>
+        </li>
+      </ul>
+      <p>Order value:</p>
+      <p>Delivery:</p>
+      <p>Total:</p>
+      <button class="btn" @click="clear">Proceed to checkout</button>
+    </section>
+    <h2 v-else>Please Make Some Orders :D</h2>
     <!-- cart item end -->
-
-    <p>Order value:</p>
-    <p>Delivery:</p>
-    <p>Total:</p>
-
-    <button class="btn" @click="clear">Proceed to checkout</button>
   </div>
 </template>
 
